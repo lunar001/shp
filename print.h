9f4549fbc49045053d9af4b88876140c47ede705
lunar001 <214607868@qq.com>
2018-10-21 11:55:34 +0800

    add the block { } for print in print.h

--- print.h
+++ print.h
@@ -7,18 +7,22 @@
 #define COLOR_BLUE  "\e[1;34m"
 
 #define log_info(fmt, ...)  \
+{
 	fprintf(stdout, COLOR_BLUE);\
 	fprintf(stdout, "log_info:%s:%s:%d:>> ", __FILE__, __func__, __LINE__);\
 	fprintf(stdout, fmt, ##__VA_ARGS__);\
 	fprintf(stdout, COLOR_NONE);
+}
 
 #ifdef D
 
 #define log_debug(fmt, ...) \
+{
 	fprintf(stdout, COLOR_BLUE);\
 	fprintf(stdout, "log_debug: %s:%s:%d: >> ", __FILE__, __func__, __LINE__);\
 	fprintf(stdout, fmt, ##__VA_ARGS__);\
 	fprintf(stdout, COLOR_NONE);
+}
 	
 
 
@@ -29,16 +33,20 @@
 #endif
 
 #define log_warning(fmt, ...) \
+{
 	fprintf(stdout, COLOR_RED); \
 	fprintf(stdout, "log_warning:%s:%s:%d:>> ", __FILE__, __func__, __LINE__);\
 	fprintf(stdout, fmt, ##__VA_ARGS__); \
 	fprintf(stdout, COLOR_NONE);
+}
 
 
 #define log_error(fmt, ...) \
+{
 	fprintf(stderr, COLOR_RED);\
 	fprintf(stderr, "log_error:%s:%s:%d:>> ", __FILE__, __func__, __LINE__);\
 	fprintf(stderr, fmt, ##__VA_ARGS__);\
 	fprintf(stdout, COLOR_NONE);
+}
 
 #endif /* #ifndef COMMON_H */
